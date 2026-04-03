82e54f15d88cb7098d1a7db56e1c210bec1e2f6a
h4ckxel <acxel.programming@gmail.com>
2026-04-03 13:43:21 -0600

    Added: Versión 2.0 de UnoGame

--- UnoGame/src/main.java
+++ UnoGame/src/main.java
@@ -2,8 +2,8 @@ import clases.Juego;
 
 public class main {
 
-	public static void main(String[] args) {
-		Juego juego = new Juego();
-		juego.iniciar();
-	}
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+        juego.iniciar();
+    }
 }
